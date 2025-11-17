f44f2d0e27da5131a37526b2305e50a3b5a85653
summydev <adegbitesumayah@gmail.com>
2025-11-17 14:41:31 +0100

    Remove poetry and use pip only

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Exit on error
-set -o errexit
-
-# Apply any outstanding database migrations
-python manage.py migrate
-
-# Build static files
-python manage.py collectstatic --noinput
