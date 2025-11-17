54bebffb9607d639dd5b1bd7df874a0b5595e645
summydev <adegbitesumayah@gmail.com>
2025-11-17 16:49:01 +0100

    Disable admin to fix template error

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -19,7 +19,7 @@ ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
